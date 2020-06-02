ed61d82d42b421e8cbad4ddab9c24ce722fad6d1
ales-blaze <alesblaze@gmail.com>
2020-06-02 15:20:54 +0530

    Changes to the views

--- app/src/main/java/com/project/android_swipes/ImageViewAdapter.java
+++ app/src/main/java/com/project/android_swipes/ImageViewAdapter.java
@@ -1,6 +1,84 @@
 package com.project.android_swipes;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
 
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
 public class ImageViewAdapter extends PagerAdapter {
+    private List<Integer> mImageId;
+    private List<String> mCardTitle;
+    private List<String> mCardText;
+    private Context mContext;
+
+    public ImageViewAdapter(List<Integer> imageId, List<String> cardTitle,List<String> cardText , Context context) {
+        mImageId = imageId;
+        mCardTitle = cardTitle;
+        mCardText = cardText;
+        mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return mImageId.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view==object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        final View view = LayoutInflater.from(mContext).inflate(R.layout.view_pager_item, container, false);
+        final Integer imageId = mImageId.get(position);
+        final String title = mCardTitle.get(position);
+        final String text = mCardText.get(position);
+        setViews(view,imageId, title, text);
+        container.addView(view);
+        return view;
+    }
+
+    private void setViews(View parent, Integer imageId, String title, String text) {
+        ImageView view1 = parent.findViewById(R.id.card_image);
+        TextView view2 = parent.findViewById(R.id.card_title_text);
+        TextView view3 = parent.findViewById(R.id.card_body_text);
+
+        view1.setVisibility(View.INVISIBLE);
+        view2.setVisibility(View.INVISIBLE);
+        view3.setVisibility(View.INVISIBLE);
+//        if (text == null && imageId == null) {
+//            view2.setVisibility(View.VISIBLE);
+//            view2.setText("No Text or Image Found");
+//        }
+//        else {
+            if (imageId != null) {
+                view1.setVisibility(View.VISIBLE);
+                Glide.with(mContext).load(imageId).into(view1);
+            }
+            if (title != null) {
+                view2.setVisibility(View.VISIBLE);
+                view2.setText(title);
+            }
+            if (text != null) {
+                view3.setVisibility(View.VISIBLE);
+                view3.setText(text);
+            }
+//        }
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
 }
--- app/src/main/java/com/project/android_swipes/MainActivity.java
+++ app/src/main/java/com/project/android_swipes/MainActivity.java
@@ -1,99 +1,41 @@
 package com.project.android_swipes;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
-import android.widget.ImageView;
+import androidx.viewpager.widget.ViewPager;
 
-import com.bumptech.glide.Glide;
+import android.os.Bundle;
 
 import java.util.ArrayList;
-import java.util.Random;
 
-public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
-    private float startX, endX, startY, endY;
-    private static final int MIN_SWIPE_DISTANCE = 50;
-    private GestureDetector mGestureDetector;
+public class MainActivity extends AppCompatActivity {
+    static ArrayList<Integer> imageIds = new ArrayList<>();
+    static ArrayList<String> cardTitle = new ArrayList<>();
+    static ArrayList<String> cardText = new ArrayList<>();
+    static {
+        imageIds.add(0,R.drawable.photo1);
+        imageIds.add(1,R.drawable.photo2);
+//        imagesName.add(2,R.drawable.photo3);
+        imageIds.add(2,null);
+        imageIds.add(3,R.drawable.photo4);
+        cardTitle.add("This is my Text here");
+        cardTitle.add(null);
+        cardTitle.add(null);
+        cardTitle.add("This is another Text here here text wraps also some more sample text here , to test out how it looks on the screen");
+
+        cardText.add("Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what topic the random paragraph will be about when it appears. This forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph as the first one of a short story and build upon it. A second option is to use the random paragraph somewhere in a short story they create. The third option is to have the random paragraph be the ending paragraph in a short story. No matter which of these challenges is undertaken, the writer is forced to use creativity to incorporate the paragraph into their writing.");
+        cardText.add("Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what topic the random paragraph will be about when it appears. This forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph as the first one of a short story and build upon it. A second option is to use the random paragraph somewhere in a short story they create. The third option is to have the random paragraph be the ending paragraph in a short story. No matter which of these challenges is undertaken, the writer is forced to use creativity to incorporate the paragraph into their writing.");
+        cardText.add("Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what topic the random paragraph will be about when it appears. This forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph as the first one of a short story and build upon it. A second option is to use the random paragraph somewhere in a short story they create. The third option is to have the random paragraph be the ending paragraph in a short story. No matter which of these challenges is undertaken, the writer is forced to use creativity to incorporate the paragraph into their writing.");
+        cardText.add("Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what topic the random paragraph will be about when it appears. This forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph as the first one of a short story and build upon it. A second option is to use the random paragraph somewhere in a short story they create. The third option is to have the random paragraph be the ending paragraph in a short story. No matter which of these challenges is undertaken, the writer is forced to use creativity to incorporate the paragraph into their writing.");
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mGestureDetector = new GestureDetector(this,this);
-    }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        mGestureDetector.onTouchEvent(event);
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                startX = event.getX();
-                startY = event.getY();
-                break;
-            case MotionEvent.ACTION_UP:
-                endX = event.getX();
-                endY = event.getY();
-
-                float directionX = endX - startX;
-                float directionY = endY - startY;
-
-                if (Math.abs(directionX) >= MIN_SWIPE_DISTANCE) {
-                    if (endX != startX){
-                        doImageLoading();
-                    }
-                }else if(Math.abs(directionY) >= MIN_SWIPE_DISTANCE) {
-
-                }
-        }
-        return true;
-    }
-
-    private void doImageLoading() {
-        Random randomNumberGenerator = new Random();
-        int index = randomNumberGenerator.nextInt(imagesName.size());
-        int imageId = 0;
-        if (index < imagesName.size() && index >= 0 ){
-            final String imageName = imagesName.get(index);
-            imageId = getResources().getIdentifier(imageName, "drawable",getPackageName());
-        }
-        ImageView cardImage = findViewById(R.id.images);
-        Glide.with(this).load(imageId).into(cardImage);
-    }
-    static ArrayList<String> imagesName = new ArrayList<>();
-    static {
-        imagesName.add(0,"photo1");
-        imagesName.add(1,"photo2");
-        imagesName.add(2,"photo3");
-        imagesName.add(3,"photo4");
-    }
-    @Override
-    public boolean onDown(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public void onShowPress(MotionEvent e) {
-
-    }
-
-    @Override
-    public boolean onSingleTapUp(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        return false;
-    }
-
-    @Override
-    public void onLongPress(MotionEvent e) {
-
-    }
-
-    @Override
-    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        return false;
+        ViewPager viewPager = findViewById(R.id.pager);
+        ImageViewAdapter imageViewAdapter = new ImageViewAdapter(imageIds, cardTitle, cardText,this);
+        viewPager.setAdapter(imageViewAdapter);
     }
 }
